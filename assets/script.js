3658d3d74d9093e0c001377abbdca76b15382c60
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-06-13 00:13:40 +1000

    added scoreCount

--- assets/script.js
+++ assets/script.js
@@ -15,38 +15,44 @@ var questionNumber; //defines which question to start
 // start
 //  quizFeedback
 
-//Timer function 
+//Timer function
 function startTimer() {
-    timerCount = 45; //start the timer from 45 
+  timerCount = 45; //start the timer from 45
+  document.querySelector("#timer").textContent = timerCount;
+  timer = setInterval(function () {
+    timerCount--; //decreases to 0
     document.querySelector("#timer").textContent = timerCount;
-    timer = setInterval(function() {
-        timerCount --; //decreases to 0
-        document.querySelector("#timer").textContent = timerCount;
-        if (timerCount <=0) { //When the timer runs out will go to the score page 
-            goScorePage();
-        }
-    }, 1000);
+    if (timerCount <= 0) {
+      //When the timer runs out will go to the score page
+      goScorePage();
+    }
+  }, 1000);
 }
 
 //Start Quiz function
-//Start Questions after clicking the start button 
+//Start Questions after clicking the start button
 function startQuestions() {
-scoreCount= 0,
-questionNumber= 0, 
-document.querySelector("#start-quiz").setAttribute("class", "hide");
-document.querySelector("#questions").setAttribute("class", "visible");
-getQuestionsText();
-startTimer();
-console.log("timer has started");
+  (scoreCount = 0),
+    (questionNumber = 0),
+    document.querySelector("#start-quiz").setAttribute("class", "hide");
+  document.querySelector("#questions").setAttribute("class", "visible");
+  getQuestionsText();
+  startTimer();
+  console.log("timer has started");
 }
 //Question text function from the questions array
 //uses document.querySelector("#question-title")' which selects the HTML #question-title to display in <h2>
-function getQuestionsText() { 
-  document.querySelector("#question-title").textContent = questions [questionNumber].question; 
-  document.querySelector("#btnAnswer0").textContent = questions[questionNumber].answer0; // similarly this uses the query.selector to select the HTMl elements for the answers 
-  document.querySelector("#btnAnswer1").textContent = questions[questionNumber].answer1;//questions[questionNumber].answer1; sets the corresponding answer choice from the questions array 
-  document.querySelector("#btnAnswer2").textContent = questions[questionNumber].answer2;
-  document.querySelector("#btnAnswer3").textContent = questions[questionNumber].answer3;
+function getQuestionsText() {
+  document.querySelector("#question-title").textContent =
+    questions[questionNumber].question;
+  document.querySelector("#btnAnswer0").textContent =
+    questions[questionNumber].answer0; // similarly this uses the query.selector to select the HTMl elements for the answers
+  document.querySelector("#btnAnswer1").textContent =
+    questions[questionNumber].answer1; //questions[questionNumber].answer1; sets the corresponding answer choice from the questions array
+  document.querySelector("#btnAnswer2").textContent =
+    questions[questionNumber].answer2;
+  document.querySelector("#btnAnswer3").textContent =
+    questions[questionNumber].answer3;
 }
 
 //Feedback for each answer prompt
@@ -54,25 +60,28 @@ function getQuestionsText() {
 function quizFeedback(text) {
   document.querySelector("#quizfeedback").textContent = text;
   document.querySelector("#quizfeedback").setAttribute("class", "visible");
-  setTimeout(function() {
+  setTimeout(function () {
     document.querySelector("#quizfeedback").setAttribute("class", "hide");
-},  
-  2000);
+  }, 2000);
 }
 
 //Function that checks checks if input was correct, updates scores counter and moves to the next question.
 function questionsAnswer(answer) {
-  if (questions[questionNumber]["answer" + answer] === questions[questionNumber].correctAnswer) { //first selects the question object based on the questionNumber array starting at 0, 
+  if (
+    questions[questionNumber]["answer" + answer] ===
+    questions[questionNumber].correctAnswer
+  ) {
+    //first selects the question object based on the questionNumber array starting at 0,
     //["answer" + answer] this here will create a string concatentation of "answer3" and with questions[questionNumber]["answer3"] will retrieve "Arrays"
     console.log("Correct!");
-    //SFX correct 
-    quizFeedback("Correct!")
+    //SFX correct
+    quizFeedback("Correct!");
     scoreCount++;
     questionNumber++;
   } else {
     console.log("Wrong!");
-    quizFeedback("Wrong!")
-    //sfx incorrect 
+    quizFeedback("Wrong!");
+    //sfx incorrect
     timerCount -= 10; // Reduce timer by ten seconds on wrong answer
     document.querySelector("#timer").textContent = timerCount; // Updates timer
     questionNumber++;
@@ -88,37 +97,51 @@ function goScorePage() {
   console.log("You are at the Score page");
   clearInterval(timer);
   document.querySelector("#questions").setAttribute("class", "hide");
-  document.querySelector("#score-screen").setAttribute("class","visible")
-  document.querySelector("#final-score").textContent = timerCount; ", with " + scoreCount + "out of" + questions.length + "correct answers!";
-} 
+  document.querySelector("#score-screen").setAttribute("class", "visible");
+  document.querySelector("#final-score").textContent =
+    timerCount +
+    ", with " +
+    scoreCount +
+    " out of " +
+    questions.length +
+    " correct answers! ";
+}
 
 
-//Event Listerners 
 
-//Start Button 
-document.querySelector("#start").addEventListener("click", function() {
-    console.log("Start button clicked");
-    startQuestions();
+//Event Listerners
+//Start Button
+document.querySelector("#start").addEventListener("click", function () {
+  console.log("Start button clicked");
+  startQuestions();
 });
-// Answer questions button 
-document.querySelector("#btnAnswer0").addEventListener("click", function(event) {
-  event.preventDefault();
-  console.log("Answer 0 button clicked");
-  questionsAnswer(0)
-});
+// Answer questions button
+document
+  .querySelector("#btnAnswer0")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Answer 0 button clicked");
+    questionsAnswer(0);
+  });
 
-document.querySelector("#btnAnswer1").addEventListener("click", function(event) {
-  event.preventDefault();
-  console.log("Answer 1 button clicked");
-  questionsAnswer(1)
-});
-document.querySelector("#btnAnswer2").addEventListener("click", function(event) {
-  event.preventDefault();
-  console.log("Answer 2 button clicked");
-  questionsAnswer(2)
-});
-document.querySelector("#btnAnswer3").addEventListener("click", function(event) {
-  event.preventDefault();
-  console.log("Answer 3 button clicked");
-  questionsAnswer(3);
-});
+document
+  .querySelector("#btnAnswer1")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Answer 1 button clicked");
+    questionsAnswer(1);
+  });
+document
+  .querySelector("#btnAnswer2")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Answer 2 button clicked");
+    questionsAnswer(2);
+  });
+document
+  .querySelector("#btnAnswer3")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Answer 3 button clicked");
+    questionsAnswer(3);
+  });
